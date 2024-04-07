936ad54ac9d239c9c57d0b59e6ffc3f1d6d1e7d4
Cesar1117 <166337152+Cesar1117@users.noreply.github.com>
2024-04-07 17:29:03 -0400

    Add files via upload

---
+++ P1LAB1_FloresCesar.py
@@ -0,0 +1,5 @@
+print('Enter your first name:', end='')
+first_name = input()
+print ('Enter your last name:', end='')
+last_name = input()
+print('Hello,', first_name, last_name,'! Welcome to CTI-110')
