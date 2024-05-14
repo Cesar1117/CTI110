ef1f3d98503dd68de4ccfb75c421302f75c15daf
Cesar1117 <166337152+Cesar1117@users.noreply.github.com>
2024-05-14 15:07:30 -0500

    Add files via upload

---
+++ P4LAB2_FloresCesar.py
@@ -0,0 +1,15 @@
+#Flores Cesar
+#04/28/2024
+#P4LAB2
+#Use while loop and for loop togther
+run_again = 'yes'
+while run_again != "no":
+  user_num = int(input("Enter an integer:"))
+  if user_num >= 0:
+   for item in range(1, 13):
+    print(f"{user_num} * {item} = {user_num * item}")
+  else:
+   print("This program does not handle negative values")
+
+  run_again = input("Would you to run the program again?")
+  print("Program is ending....")
